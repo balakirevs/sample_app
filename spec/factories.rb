677e3fdf4edadc21ef055fe3051ef7ffd50aa7c1
Alexander Balakirev <balakirevs@i.ua>
2012-02-27 14:11:57 +0200

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+ user.name                   "Alexander Balakirev"
+ user.email                  "abv@example.com"
+ user.password               "foobar"
+ user.password_confirmation  "foobar"
+end 
