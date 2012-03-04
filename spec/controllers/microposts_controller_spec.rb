085f83a999d36385a67bc12c785715456ac568b4
Alexander Balakirev <balakirevs@i.ua>
2012-03-04 03:50:47 +0200

    Added user microposts

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -4,6 +4,7 @@ describe MicropostsController do
   render_views
   
   describe "access control" do
+  
     it "should deny access to 'create'" do
       post :create
       response.should redirect_to(signin_path)
